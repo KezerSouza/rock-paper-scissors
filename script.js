dcfeeead9f55a918314ef0a90f8385b825c04fbe
KezerSouza <kezer@discente.ufg.br>
2025-01-05 16:49:55 -0300

    Function to randomize computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+let computerChoice;
+
+function getComputerChoice() {
+     let random_number = Math.floor(Math.random() * 3);
+     
+     if(random_number == 0) {
+        computerChoice = "rock";
+        return;
+     }
+     if(random_number == 1) {
+        computerChoice = "paper";
+        return;
+     }
+     if(random_number == 2) {
+        computerChoice = "scissors";
+        return;
+     }
+
+}
+
+getComputerChoice();
+console.log(computerChoice);
